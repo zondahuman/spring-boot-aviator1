1c8b80fe0ad5e9c02aff36b8150ca353f744cbf9
zondahuman <zondahuman@gmail.com>
2025-05-19 21:34:34 +0800

    add

---
+++ src/test/java/com/abin/lee/aviator/test/aviator/ApacheGroovyEngineTest1.java
@@ -0,0 +1,36 @@
+package com.abin.lee.aviator.test.aviator;
+
+import org.apache.logging.log4j.core.script.Script;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApacheGroovyEngineTest1 {
+
+
+    @Test
+    public void testPlusRule(){
+        Object result1 = parse("line.contains(\"test\")", "9123rhewtestdfks");
+        Object result2 = parse("line.contains(\"test\")", "123");
+        System.out.println(result1);
+        System.out.println(result2);
+
+    }
+
+    public static Object parse(String re, String line) {
+
+        Map<String, Object> map = new HashMap<String, Object>(1);
+        map.put("line", line);
+
+        Script script = new GroovyShell().parse(re);
+        script.setBinding(new Binding(map));
+        return script.run();
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/com/abin/lee/aviator/test/aviator/CodehausGroovyEngineTest1.java
@@ -0,0 +1,37 @@
+package com.abin.lee.aviator.test.aviator;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import groovy.lang.Script;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CodehausGroovyEngineTest1 {
+
+
+    @Test
+    public void testPlusRule(){
+        Object result1 = parse("line.contains(\"test\")", "9123rhewtestdfks");
+        Object result2 = parse("line.contains(\"test\")", "123");
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+    public static Object parse(String re, String line) {
+
+        Map<String, Object> map = new HashMap<String, Object>(1);
+        map.put("line", line);
+
+        Script script = new GroovyShell().parse(re);
+        script.setBinding(new Binding(map));
+        return script.run();
+    }
+
+
+
+
+
+
+}
