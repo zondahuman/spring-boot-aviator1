e6d68a5e39f8c52c2d4f70829e9867a8ed518246
zondahuman <zondahuman@gmail.com>
2025-05-19 11:41:39 +0800

    add

--- src/test/java/com/abin/lee/aviator/test/aviator/AviatorEngineTest1.java
+++ src/test/java/com/abin/lee/aviator/test/aviator/AviatorEngineTest1.java
@@ -3,6 +3,8 @@ package com.abin.lee.aviator.test.aviator;
 import com.googlecode.aviator.AviatorEvaluator;
 import com.googlecode.aviator.Expression;
 import com.googlecode.aviator.runtime.function.AbstractFunction;
+import com.googlecode.aviator.runtime.function.FunctionUtils;
+import com.googlecode.aviator.runtime.type.AviatorBigInt;
 import com.googlecode.aviator.runtime.type.AviatorObject;
 import org.junit.Test;
 
@@ -135,6 +137,7 @@ public class AviatorEngineTest1 {
     @Test
     public void testCompileRule2(){
         String expression = "a-(b-c)>100";
+//        Expression compiledExp = AviatorEvaluator.compile(expression);
         Expression compiledExp = AviatorEvaluator.compile(expression, true);
         Map<String, Object> env = new HashMap<>();
         env.put("a", 150);
@@ -153,11 +156,29 @@ public class AviatorEngineTest1 {
      */
     @Test
     public void testSupportSelfDefineFunction(){
-
+        AviatorEvaluator.addFunction(new MinFunction());
+        String expression = "min(a,b)";
+        Expression compiledExp = AviatorEvaluator.compile(expression, true);
+        Map<String, Object> env = new HashMap<>();
+        env.put("a", 100.3);
+        env.put("b", 45);
+        Double result = (Double) compiledExp.execute(env);
+        System.out.println(result);
     }
 
+    static class MinFunction extends AbstractFunction {
+        @Override public AviatorObject call(Map<String, Object> env, AviatorObject arg1, AviatorObject arg2) {
+            Number left = FunctionUtils.getNumberValue(arg1, env);
+            Number right = FunctionUtils.getNumberValue(arg2, env);
+            return new AviatorBigInt(Math.min(left.doubleValue(), right.doubleValue()));
+        }
 
+        public String getName() {
+            return "min";
+        }
+    }
 
+    
 
 
 
